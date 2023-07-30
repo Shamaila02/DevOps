4fc92a51a15a9c7fd33a8f9aaee84828fdc66d0c
Shamaila02 <2021.shamaila.ansari@ves.ac.in>
2023-07-30 23:28:05 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+class Multiply{
+    public static void main(String a[]){
+        int num1 = 90;
+        int num2= 10;
+        int multiply = num1*num2;
+        System.out.println("Multiplication:",+multiply);
+    }
+}
