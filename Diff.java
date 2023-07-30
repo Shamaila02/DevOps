990203d93bbb9fe91efb699d75191825c66dceeb
Shamaila02 <2021.shamaila.ansari@ves.ac.in>
2023-07-30 22:36:37 +0530

    added message

---
+++ Diff.java
@@ -0,0 +1,8 @@
+class Diff{
+    public static void main(String a[]){
+        int num1 = 90;
+        int num2= 10;
+        int diff = num1-num2;
+        System.out.println("Difference:"+diff);
+    }
+}
